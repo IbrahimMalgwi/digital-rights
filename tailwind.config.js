e29fad9b2b27788a928d3bf1bd40dda57734c164
IbrahimMalgwi <ibrahimganamalgwi@outlook.com>
2026-06-09 22:24:24 +0100

    update deployment routing

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,4 @@
-/** @type {import('tailwindcss').Config} */
+ /** @type {import('tailwindcss').Config} */
 module.exports = {
     content: [
         "./src/**/*.{js,jsx,ts,tsx}",
@@ -80,3 +80,4 @@ module.exports = {
     },
     plugins: [],
 }
+
